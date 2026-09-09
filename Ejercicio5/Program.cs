2159174f1bc86c612c26449280d9429f2861e48e
ElBumer101 <josepeduardo101vazquez@gmail.com>
2026-09-09 17:06:18 -0600

    implement switch expression with relational patterns for score evaluation
    Se agregó un nuevo ejercicio de clasificación de calificaciones usando
    características modernas de C#:
    - Declaración de variables locales usando inferencia de tipos (var).
    - Implementación de una expresión switch que devuelve una cadena
    anulable (string?).
    - Uso de coincidencia de patrones relacionales (>= 95, >= 85, >= 70, <
    70) para evaluar rangos numéricos de forma eficiente.
    - Formateo de salida limpia en consola usando interpolación de cadenas.

---
+++ Ejercicio5/Program.cs
@@ -0,0 +1,12 @@
+﻿var nombreEstudiante = "Thorffinth";
+var calificacion = 100;
+
+string? verificar = calificacion switch
+{
+    >= 95 => "Calificacion perfecta. Felicidades",
+    >= 85 => "Calificacion buena",
+    >= 70 => "Calificacion pasable",
+    < 70 => "Date de baja"
+};
+
+Console.WriteLine($" {nombreEstudiante}. Los resultados de tu calificaico: {verificar} ");
