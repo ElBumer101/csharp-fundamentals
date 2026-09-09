157dbc9b8b4ecabf2a7b847445baeee29ad83fac
ElBumer101 <josepeduardo101vazquez@gmail.com>
2026-09-09 16:31:38 -0600

    convert Exercise 3 into an interactive menu using switch statements
    Refactored Exercise 3 to replace static logic with a fully interactive
    console menu:
    - Maintained the original base text context while introducing user
    interaction.
    - Captured user input using string? and Console.ReadLine().
    - Implemented control flow using a switch statement to handle option
    selection (cases 1-3).
    - Added a default case with an early return statement to handle invalid
    entries cleanly.
    - Printed a dynamic final confirmation message with interpolated output.

--- Ejercicio3/Program.cs
+++ Ejercicio3/Program.cs
@@ -1,4 +1,32 @@
-﻿var  datos = "Manzanas, sandias, uvas";
-string[] partes = datos.Split(',');
-Console.WriteLine($"Encuentra: {partes[0]}, {partes[1]}, {partes[2]}");
+﻿Console.WriteLine("========================");
+Console.WriteLine($"¿Que quieres comer?  ");
+Console.WriteLine("1. Comer Manzanas");
+Console.WriteLine("2. Comer Sandias");
+Console.WriteLine("3. Comer Uvas");
+Console.WriteLine("========================");
+Console.Write("Elige una opción (1-3): ");
+string? interaccion = Console.ReadLine();
+
+Console.WriteLine("========================");
+
+switch (interaccion)
+{
+    case "1":
+        Console.WriteLine("Opcion 1 = Comeras una manzana");
+        break;  
+
+    case "2":
+        Console.WriteLine("Opcion 2 = Comeras una sandia");
+        break;
+
+    case "3":
+        Console.WriteLine("Opcion 3 = Comeras Uvas");
+        break;
+
+    default:
+        Console.WriteLine("Obcion no valida, por favor elija un numero");
+        return;
+
+}
+Console.WriteLine($"Elegiste la opcion: {interaccion}. ¡Buen provecho!");
 
