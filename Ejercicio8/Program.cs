ccb13f3ce58dd0f259ce5d8e14c3f2a4747ef2b5
ElBumer101 <josepeduardo101vazquez@gmail.com>
2026-09-11 11:30:34 -0600

    simplify string comparison logic in Exercise 8 terms check
    Removed redundant uppercase check on .ToLower() string comparison while
    keeping early exit guard clauses intact.

---
+++ Ejercicio8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.InteropServices;
+
+Console.WriteLine("Iniciando programa...");
+Console.Write("¿Aceptas los terminos y condiciones? S/N: ");
+string? verificarUsuario = Console.ReadLine();
+
+if (verificarUsuario.ToLower() != "s")
+{
+    Console.WriteLine("No aceptaste los terminos y condiciones.");
+    return;
+}
+Console.WriteLine("Aceptaste los terminos y condicones");
+
+Console.WriteLine("=================");
+
+Console.WriteLine("Escribe la contraseña: ");
+var constraseñaUsuario = "123admin";
+string? verificarContraseña = Console.ReadLine();
+if (verificarContraseña != constraseñaUsuario)
+{
+    Console.WriteLine("Tu contraseña es incorrecta");
+    return;
+}
+Console.WriteLine("Tu contraseña es correcta. Bienvenido");
