aa2b7d7017abc437b94b08374ceb4d5fbd280f61
ElBumer101 <josepeduardo101vazquez@gmail.com>
2026-09-12 22:01:33 -0600

    implement Exercise 11 health bar simulation with conditional while loop (basic) v1
    Implemented character health reduction logic using a condition-driven
    while loop (vidaAliado > 0). Integrated int.TryParse for safe damage
    input processing and compound subtraction assignment operator (-=).

---
+++ Ejercicio11/Program.cs
@@ -0,0 +1,20 @@
+﻿var vidaAliado = 100;
+Console.Write("¿Cual es tu nombre? ");
+string? nombreUsuario = Console.ReadLine();
+Console.WriteLine($"¡Bienvenido! {nombreUsuario}");
+
+while (vidaAliado >0)
+{
+    Console.WriteLine($"Vida actual {vidaAliado} ");
+    Console.Write($"¿Cuano daño recibio? {nombreUsuario}");
+    string? veriicar = Console.ReadLine();
+    if (int.TryParse(veriicar, out int daño))
+    {
+        vidaAliado -= daño;
+    }
+    else
+    {
+        Console.WriteLine($"Por favor, ponga un numero valido. {nombreUsuario}");
+    }
+}
+Console.WriteLine($"{nombreUsuario}Tu personaje a muerto");
